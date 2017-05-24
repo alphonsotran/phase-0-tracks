5674f774bd495ad75949fc75c560a70e78286af2
Alphonso Tran <alphonsotran@gmail.com>
2017-05-23 23:29:48 -0700

    Comment out old code. Update code with Release 3. Complete.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,45 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#  def self.yelling_happily(words)
+#  	words + "!!!" + " :)"
+#  end
+#end
+#
+#
+##Driver Code
+##Release 1
+#puts Shout.yell_angrily("Yesss")
+#puts Shout.yelling_happily("Yesss")
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :)"
-  end
+module Shout
+
+	def cry
+		puts "WAAAAAAA"
+	end 
+
+	def tantrum
+		puts "*Throws a fit*"
+	end
+
 end
 
+class Monkey
+	include Shout
+end
+
+class Human_Baby
+	include Shout
+end 
 
 #Driver Code
-#Release 1
-puts Shout.yell_angrily("Yesss")
-puts Shout.yelling_happily("Yesss")
+
+chimp = Monkey.new
+chimp.cry
+chimp.tantrum
+
+baby = Human_Baby.new
+baby.cry
+baby.tantrum
