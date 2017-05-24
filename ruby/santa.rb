4006e5e9f16791c074c6da5299347324714bf1ed
alphonsotran <alphonsotran@gmail.com>
2017-05-23 17:38:32 -0700

    Update santa.rb with code. Add getter and setter method, but still testing.

---
+++ ruby/santa.rb
@@ -0,0 +1,87 @@
+class Santa
+
+  def speak
+    puts "Ho, ho ho! Haaaappy Holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
+
+  def initialize(name, gender, ethnicity, weight)
+    puts "initializing Santa instance ..."
+    @name = name
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @weight = weight
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
+
+  def about
+    puts "Name: #{@name}"
+    puts "Gender: #{@gender}"
+    puts "Ethnicity: #{@ethnicity}"
+    puts "Age: #{@age}"
+    puts "Weight: #{@weight}"
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+
+    @reindeer_ranking.each_index do |idx|
+      if @reindeer_ranking[idx] == "reindeer_name"
+        #@reindeer_ranking.slice!(idx)
+        @reindeer_ranking.push("reindeer_name")
+      end
+    end
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+# getter method
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def reindeer_rank
+    @reindeer_ranking
+  end
+end
+
+# Drive code
+
+
+
+#santa_names = ["Mickey", "Jackie", "James", "Leon", "Trinity", "Leah", "Unk"]
+#
+#santas = []
+#
+#
+#santas << Santa.new("Mickey", "agender", "black", 150)
+#santas << Santa.new("Jackie", "female", "Latino", 140)
+#santas << Santa.new("James", "bigender", "white", 200)
+#santas << Santa.new("Leon", "male", "Japanese", 180)
+#santas << Santa.new("Trinity", "female", "prefer not to say", 105)
+#santas << Santa.new("Leah", "gender fluid", "Mystical Creature (unicorn)", 250)
+#santas << Santa.new("Unk", "N/A", "N/A", 175)
+#
+#santas.each do |santa|
+#  santa.about
+#  puts "----"
+#end
+
+santa_m = Santa.new("Mickey", "agender", "black", 150)
+santa_m.celebrate_birthday
+
+#santa_m.get_mad_at("Dasher")
+puts "#{santa_m.age}"
