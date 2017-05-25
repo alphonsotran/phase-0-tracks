b0063abff7078e45dbe675d660d9673d7de3bee0
alphonsotran <alphonsotran@gmail.com>
2017-05-25 15:58:10 -0700

    Confirm code working correctly.

--- ruby/WordGuess/word_guess_rspec.rb
+++ ruby/WordGuess/word_guess_rspec.rb
@@ -1,15 +1,15 @@
 require_relative 'word_guess'
 
 describe WordGuess do
-	let(:word) {WordGuess.new("hello")}
+	let(:secret) {WordGuess.new("hello")}
 
 	it "converts number of words to '-'" do
-		expect(word.word_convert).to eq "-----"
+		expect(secret.word_convert).to eq "-----"
 	end
 
 	it "replaces '-' with correct letter" do
-		word.word_convert
-		expect(word.guess("h")).to eq "h----"
+		secret.word_convert
+		expect(secret.guess("h")).to eq "h----"
 	end
 
 end
