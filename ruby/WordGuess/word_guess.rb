6092f18f64e2b014a828677b1723c0e33e4497fe
alphonsotran <alphonsotran@gmail.com>
2017-05-25 15:43:24 -0700

    Update driver code.

--- ruby/WordGuess/word_guess.rb
+++ ruby/WordGuess/word_guess.rb
@@ -1,11 +1,11 @@
 #Pseduocode
-#
+
 #Create a 'WordGuess' class. Include methods:
-#
+
 #Method that initializes and adds appropriate user input into instance variable downcase. Set instance variables for guess_count at user's inputted word's length and array. Set up instance variable with blank array.
-#
+
 #Method that converts each word to "-" by iterating through array of words and replace with "-"
-#
+
 #Method that iterates over word array and replaces "-" with appropriate index of the correct word. Decrement guess_count by one if guessed incorrectly. Converts array back to string.
 #
 #Driver code
@@ -47,16 +47,6 @@ class WordGuess
 end
 
 
-#random = WordGuess.new("hello")
-
-#p random.word_convert
-#p random.blankstr
-#random.guess("h")
-#p random.blankstr
-#random.guess("l")
-#p random.blankstr
-#p random.guess_count
-
 #Driver code
 #Ask user for word and initialize.
 puts "Input a word to have the other person guess."
@@ -68,16 +58,21 @@ random.word_convert
 
 #Create a loop asking for a letter until countdown = 0 or guessed word correctly.
 loop do
+	sleep(0.4)
 	puts " "
-	puts "#{random.blankstr}"
+	puts "Word to guess: #{random.blankstr}"
 	puts "Guess one letter at a time. Tries remaining: #{random.guess_count}"
 	letter = gets.chomp
 	random.guess(letter)
 		if random.blankstr == random.userword
-			puts "You win!"
+			puts " "
+			puts "The secret word is '#{random.userword}'"
+			puts "Congratulations! You a winner!"
 			break
 		elsif random.guess_count == 0
-			puts "You lose!"
+			puts " "
+			puts "Ha ha. You lose, loser!"
+			puts "The secret word is '#{random.userword}'"
 			break
 		else
 			next
