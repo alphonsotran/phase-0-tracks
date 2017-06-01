21a2aa09246e1be06453c38ea5efc6ac1b23752c
alphonsotran <alphonsotran@gmail.com>
2017-05-31 18:29:04 -0700

    Update with Release 1 code.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,15 @@
 var colors = ["red", "green", "blue", "yellow"];
 var names = ["Ed", "Hilbert", "Unicorn", "Pony"];
+var horses = {};
 
 colors.push("purple");
 names.push("Alex");
 
 console.log(colors);
 console.log(names);
+
+for (var i = 0; i < colors.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
