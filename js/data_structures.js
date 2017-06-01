c912304c6ae71f1f39254923b3306fedad2739ed
alphonsotran <alphonsotran@gmail.com>
2017-05-31 19:00:03 -0700

    Update with Release 2 code.

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,27 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horses);
+
+function Car(color, model, make){
+
+  this.color = color;
+  this.model = model;
+  this.make = make;
+
+  this.turbo = function () {
+    console.log("Vroom Vroom!");
+  }
+
+}
+
+var newcar = new Car("black", "Toyota", "Prius");
+console.log(newcar.color + " " + newcar.model + " " + newcar.make + ".");
+newcar.turbo();
+
+var newcar2014 = new Car("blue", "Honda", "Civic");
+console.log(newcar2014.color + " " + newcar2014.model + " " + newcar2014.make + ".");
+newcar2014.turbo();
+
+var newcar2015 = new Car("white", "Hyundai", "Something");
+console.log(newcar2015.color + " " + newcar2015.model + " " + newcar2015.make + ".");
+newcar2015.turbo();
