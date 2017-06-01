fe8e73f407a1eb72d0efe91bd00bc2433788da18
alphonsotran <alphonsotran@gmail.com>
2017-05-31 18:16:46 -0700

    Initialize and updated with Release 0 code.

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["red", "green", "blue", "yellow"];
+var names = ["Ed", "Hilbert", "Unicorn", "Pony"];
+
+colors.push("purple");
+names.push("Alex");
+
+console.log(colors);
+console.log(names);
