59321d2dc61a2364bd144ae55f20c785b8c7f7f0
alphonsotran <alphonsotran@gmail.com>
2017-05-31 17:05:27 -0700

    Add function.

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,17 @@
 // Set a 'for' loop that will start the count at the string's length. Set a conditional statement to have it be greater than -1. Set a decrement to subtract 1 from the counter each time.
 // The block of the code will push each element from the array containing the split string starting from the end to the front into the empty array.
 //After the loop ends, the new array containing the reversed letters of the string will be joined back into a string and returned.
+
+var reverse = function(word) {
+  var result = [];
+  var splitting = word.split('');
+
+  for (var i=word.length - 1; i > -1; i--) {
+    result.push(splitting[i]);
+  }
+
+  return result.join('');
+
+};
+
+console.log(reverse("thanksgiving"));
