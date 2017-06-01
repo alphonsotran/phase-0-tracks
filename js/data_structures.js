079440863aec0ef9319f0345c5448339679bc129
alphonsotran <alphonsotran@gmail.com>
2017-05-31 19:35:18 -0700

    Clean up code.

--- js/data_structures.js
+++ js/data_structures.js
@@ -37,3 +37,22 @@ newcar2014.turbo();
 var newcar2015 = new Car("white", "Hyundai", "Something");
 console.log(newcar2015.color + " " + newcar2015.model + " " + newcar2015.make + ".");
 newcar2015.turbo();
+
+/* Release 3 research with prototypes.
+Car.prototype.seats=5;
+console.log(newcar.seats);
+console.log(newcar2014.seats);
+console.log(newcar2015.seats);
+Car.prototype.seats = "fishsticks";
+console.log(newcar.seats);
+console.log(newcar2014.seats);
+console.log(newcar2015.seats);
+newcar2015.seats = 2;
+console.log(newcar.seats);
+console.log(newcar2014.seats);
+console.log(newcar2015.seats);
+
+var newcar2016 = new Car("silver", "Toyota", "Corolla");
+console.log(newcar2016.seats);
+console.log(newcar2016.hasOwnProperty('seats'));
+*/
