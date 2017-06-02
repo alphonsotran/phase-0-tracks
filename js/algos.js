5e23cfe8b95af354ccb2f6edfee2c4e32a944126
alphonsotran <alphonsotran@gmail.com>
2017-06-02 13:24:18 -0700

    Update with new pseudocode for randomizing letters.

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,12 @@
 // Pseudocode
 
-// Write a function that will loop through the array and push the length of each element to a new array. 
-// The new array containing the length of each element will then give a max value using the Math.max method and assigned a new variable. 
+// Write a function that will loop through the array and push the length of each element to a new array.
+// The new array containing the length of each element will then give a max value using the Math.max method and assigned a new variable.
 // The new variable containing the max element will then be used to find the index in the array with the length of each element. This will be assigned a new variable.
-// The new variable containing the index of the max length will then be used on the original array with the words. 
+// The new variable containing the index of the max length will then be used on the original array with the words.
+
+var test_arr = ["long phrase","aaaaaaaaaaaaaaaaaa","longest phrase", "asldkfas;ldfasldf", "longer phrase"];
+
 var longest_phrase = function(arr) {
 	var length_arr = [];
 
@@ -16,8 +19,12 @@ var longest_phrase = function(arr) {
 	return arr[longest_el];
 }
 
-// Write a function that incorportates two objects. The objects will pass through the conditional statement for 
-// each property comparing it to each other (||). If true, then return true. If nothign compares, then return false. 
+// Write a function that incorporates two objects. The objects will pass through the conditional statement for
+// each property comparing it to each other (||). If true, then return true. If nothing compares, then return false.
+
+var census1994 = {name: "Steven", age: 54};
+var census2004 = {name: "Tamir", age: 54};
+
 var match = function(object1, object2){
 	if (object1.name === object2.name || object1.age === object2.age) {
 	return true;
@@ -26,11 +33,15 @@ var match = function(object1, object2){
 	}
 }
 
-	
+// Set up two variables with empty arrays. One variable where n (counter) = 1. Another variable with the alphabet in its array.
+// set up a function called randomizer with one parameter (the number of letters in an array).
+// Within the function, set a loop where the parameter is the limit. Set the function equals to 0.
+// Within the loop, set a 'while' loop that will start counter at 1 and end at a random number assigned by Math.random(). The first loop will push a random letter from the alphabet into an empty array. This will go on until the 'while' loop closes. Afterwards, array containing the random letters will be put into a new variable where it is converted into a string.
+// The new variable containing the new string will then be pushed into another empty array. The variable array containing the randomized letters will then be emptied along with the 'while' counter. The next loop will start and the process all over again.
 
-var census1994 = {name: "Steven", age: 54};
-var census2004 = {name: "Tamir", age: 54};
-var test_arr = ["long phrase","aaaaaaaaaaaaaaaaaa","longest phrase", "asldkfas;ldfasldf", "longer phrase"];
+
+
+
 
 console.log(longest_phrase(test_arr));
 console.log(match(census1994, census2004));
