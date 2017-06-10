f91f2c6b06fd6c485761e93ede747bf029dea6f7
alphonsotran <alphonsotran@gmail.com>
2017-06-10 15:06:16 -0700

    Start driver code to allow user to manipulate data.

--- databases/medicine.rb
+++ databases/medicine.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 require 'date'
 
-db = SQLite3::Database.new("drug_regiment.db")
+db = SQLite3::Database.new("drug_regimen.db")
 db.results_as_hash = true
 
 table_creator_days = <<-SQL
@@ -19,8 +19,8 @@ table_creator_drugs = <<-SQL
     CONSTRAINT drug_name UNIQUE (name)
   )
 SQL
-table_creator_regiment = <<-SQL
-  CREATE TABLE IF NOT EXISTS regiment(
+table_creator_regimen = <<-SQL
+  CREATE TABLE IF NOT EXISTS regimen(
   id INTEGER PRIMARY KEY,
   days_id INT,
   drugs_id INT,
@@ -38,12 +38,12 @@ days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 
 db.execute(table_creator_days)
 db.execute(table_creator_drugs)
-db.execute(table_creator_regiment)
-regiment_array = db.execute("SELECT * FROM regiment")
-regiment_sat = db.execute(
+db.execute(table_creator_regimen)
+regimen_array = db.execute("SELECT * FROM regimen")
+regimen_sat = db.execute(
           "SELECT days.day, drugs.name, drugs.time FROM days
-          JOIN regiment ON days.id = regiment.days_id
-          JOIN drugs ON regiment.drugs_id = drugs.id
+          JOIN regimen ON days.id = regimen.days_id
+          JOIN drugs ON regimen.drugs_id = drugs.id
           WHERE days.id = 7"
         )
 
@@ -56,34 +56,34 @@ db.execute("INSERT OR IGNORE INTO drugs (name, time) VALUES ('Multivitamins', '1
 # Populate data under days table
 days_of_week.each {|d| add_days(db, d)}
 
-# Populate data under regiment table
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (2, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (3, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (4, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (5, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 3)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 2)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 2)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 1)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 1)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 1)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (2, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (3, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (4, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (5, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 4)")
-db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 4)")
+# Populate data under regimen table
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (1, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (2, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (3, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (4, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (5, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (6, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (7, 3)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (6, 2)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (7, 2)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (1, 1)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (6, 1)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (7, 1)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (1, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (2, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (3, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (4, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (5, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (6, 4)")
+db.execute("INSERT OR IGNORE INTO regimen (days_id, drugs_id) VALUES (7, 4)")
 
 
 
 
 
 
-#p regiment_array
-#p regiment_sat
+#p regimen_array
+#p regimen_sat
 
 # Driver code
 
@@ -91,74 +91,87 @@ puts "What would you like to do? Type 'Monday', 'Tuesday', etc."
 user_prompt = gets.chomp.downcase
 today = Time.now
 
-  if user_prompt == "sunday" || (user_prompt = "today" && today.sunday?)
+  if user_prompt == "sunday"
     sun_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 1"
     ).sort_by {|k| k['time']}
+
+    puts "For Sunday's regimen:"
     sun_schedule.each do |k|
-      puts "For Sunday's "
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "monday" || (user_prompt = "today" && today.monday?)
+  elsif user_prompt == "monday"
     mon_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 2"
     ).sort_by {|k| k['time']}
+
+    puts "For Monday's regimen:"
     mon_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "tuesday" || (user_prompt = "today" && today.tuesday?)
+  elsif user_prompt == "tuesday"
     tue_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 3"
     ).sort_by {|k| k['time']}
+
+    puts "For Tuesday's regimen:"
     tue_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "wednesday" || (user_prompt = "today" && today.wednesday?)
+  elsif user_prompt == "wednesday"
     wed_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 4"
     ).sort_by {|k| k['time']}
+
+    puts "For Wednesday's regimen:"
     wed_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "thursday" || (user_prompt = "today" && today.thursday?)
+  elsif user_prompt == "thursday"
     thu_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 5"
     ).sort_by {|k| k['time']}
+
+    puts "For Thursday's regimen:"
     thu_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "friday" || (user_prompt = "today" && today.friday?)
+  elsif user_prompt == "friday"
     fri_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 6"
     ).sort_by {|k| k['time']}
+
+    puts "For Friday's regimen:"
     fri_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
-  elsif user_prompt == "saturday" || (user_prompt = "today" && today.saturday?)
+  elsif user_prompt == "saturday"
     sat_schedule = db.execute(
       "SELECT days.day, drugs.name, drugs.time FROM days
-      JOIN regiment ON days.id = regiment.days_id
-      JOIN drugs ON regiment.drugs_id = drugs.id
+      JOIN regimen ON days.id = regimen.days_id
+      JOIN drugs ON regimen.drugs_id = drugs.id
       WHERE days.id = 7"
     ).sort_by {|k| k['time']}
+
+    puts "For Saturday's regimen:"
     sat_schedule.each do |k|
       puts "#{k['name']} at #{k['time']}."
     end
@@ -166,3 +179,17 @@ today = Time.now
     puts "Invalid input."
   end
 
+#puts "Would you like to create or delete a regimen?"
+#user_creation = gets.chomp.downcase
+
+puts "Type name of medicine:"
+pill_name = gets.chomp.downcase
+
+puts "Type time of dosage: (use military hours e.g. 22:00)"
+dosage_time = gets.chomp
+
+puts "What day will you take the medicine?"
+what_day = gets.chomp.downcase
+
+db.execute("INSERT INTO drugs (name, time) VALUE (#{pill_name}, #{dosage_time})")
+db.execute("INSERT INTO regimen (days_id, drugs_id) VALUES (7, NULL)")
