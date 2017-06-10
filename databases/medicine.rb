147f0a7f997c483234d59dcff5c46df03669333b
alphonsotran <alphonsotran@gmail.com>
2017-06-10 12:10:27 -0700

    Populate tables with data.

--- databases/medicine.rb
+++ databases/medicine.rb
@@ -7,15 +7,16 @@ db.results_as_hash = true
 table_creator_days = <<-SQL
   CREATE TABLE IF NOT EXISTS days(
     id INTEGER PRIMARY KEY,
-    day VARCHAR(255)
+    day VARCHAR(255),
+    CONSTRAINT week UNIQUE (day)
   )
 SQL
 table_creator_drugs = <<-SQL
   CREATE TABLE IF NOT EXISTS drugs(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    time_per_day INT,
-    time INT
+    time INT,
+    CONSTRAINT drug_name UNIQUE (name)
   )
 SQL
 table_creator_regiment = <<-SQL
@@ -24,22 +25,63 @@ table_creator_regiment = <<-SQL
   days_id INT,
   drugs_id INT,
   FOREIGN KEY (days_id) REFERENCES days(id),
-  FOREIGN KEY (drugs_id) REFERENCES drugs(id)
+  FOREIGN KEY (drugs_id) REFERENCES drugs(id),
+  CONSTRAINT match_id UNIQUE (days_id, drugs_id)
   )
 SQL
+
+def add_days(db, date)
+  db.execute("INSERT OR IGNORE INTO days (day) VALUES (?)", [date])
+end
+
+days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 db.execute(table_creator_days)
 db.execute(table_creator_drugs)
 db.execute(table_creator_regiment)
 regiment_array = db.execute("SELECT * FROM regiment")
 
-days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+# Populate data under drugs table
+db.execute("INSERT OR IGNORE INTO drugs (name, time) VALUES ('Benadryl', '8:00')")
+db.execute("INSERT OR IGNORE INTO drugs (name, time) VALUES ('Melatonin', '23:00')")
+db.execute("INSERT OR IGNORE INTO drugs (name, time) VALUES ('Chlorthalidone', '10:00')")
+db.execute("INSERT OR IGNORE INTO drugs (name, time) VALUES ('Multivitamins', '12:00')")
 
-def add_days(db, date)
-  db.execute("INSERT OR IGNORE INTO days (day) VALUES (?)", [date])
-end
-
-db.execute("INSERT OR IGNORE INTO drugs (name, time_per_day, time) VALUES ('Fiber Pills', 2, '22:00')")
-
+# Populate data under days table
 days_of_week.each {|d| add_days(db, d)}
-p regiment_array
+
+# Populate data under regiment table
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (2, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (3, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (4, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (5, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 3)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 2)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 2)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 1)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 1)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 1)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (1, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (2, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (3, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (4, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (5, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (6, 4)")
+db.execute("INSERT OR IGNORE INTO regiment (days_id, drugs_id) VALUES (7, 4)")
+
+
+
+
+
+
+#p regiment_array
+
+# Driver code
+
+puts "What would you like to do? Type 'today' for today's regiment.
+today = gets.chomp
+
+
+
